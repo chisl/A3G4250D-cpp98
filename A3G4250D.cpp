8c97ce96afa01647a5e2fde3bf20afa0fdf60f9e
chisl <info@chisl.io>
2018-01-24 08:59:49 +0100

    Initial commit

---
+++ A3G4250D.cpp
@@ -0,0 +1,18 @@
+/*
+ * name:        A3G4250D
+ * description: MEMS motion sensor: 3-axis digital output gyroscope
+ * manuf:       STMicroelectronics
+ * version:     Version 0.1
+ * url:         http://www.st.com/resource/en/datasheet/a3g4250d.pdf
+ * date:        2018-01-23
+ * author       https://chisl.io/
+ * file:        A3G4250D.cpp
+ */
+
+/*                                                                                                       *
+ *                                   THIS FILE IS AUTOMATICALLY CREATED                                  *
+ *                                    D O     N O T     M O D I F Y  !                                   *
+ *                                                                                                       */
+
+#include "A3G4250D.hpp"
+
